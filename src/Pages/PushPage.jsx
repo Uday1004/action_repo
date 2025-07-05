2cf891e890ae374353436ec6f3ea42e48c75684c
Uday Solanki <Udaysolanki530@gmail.com>
2025-07-05 08:43:54 +0530

    testing push

--- src/Pages/PushPage.jsx
+++ src/Pages/PushPage.jsx
@@ -5,7 +5,7 @@ function PushPage() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h2>Push Testing Page</h2>
+      <h2>Push Testing Page to GitHub</h2>
       <textarea
         rows={10}
         cols={50}
